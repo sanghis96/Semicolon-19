ba06355b06d46916041c1e231887bc44cd9416be
gitiviti <vivek.jain0906@gmail.com>
2019-03-05 03:52:11 +0530

    Camera voice command changes

--- app/src/main/java/com/example/team/semicolon_19/CameraActivity.java
+++ app/src/main/java/com/example/team/semicolon_19/CameraActivity.java
@@ -6,7 +6,13 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,17 +25,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 import static android.content.ContentValues.TAG;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends Activity implements RecognitionListener {
 
     private Camera mCamera;
     private CameraPreview mPreview;
     public static final int MEDIA_TYPE_IMAGE = 1;
+    public TextToSpeech t1;
+    public SpeechRecognizer speech;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,20 +65,14 @@ public class CameraActivity extends Activity {
 
         System.out.println("Samarth 3");
 
-        // Add a listener to the Capture button
-        Button captureButton = (Button) findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        System.out.println("Samarth 4");
-                        // get an image from the camera
-                        mCamera.takePicture(null, null, mPicture);
-                        System.out.println("Samarth 5");
-                    }
-                }
-
-        );
+        Download d = new Download();
+        String s = null;
+        try {
+            s = d.execute().get();
+        }
+        catch(Exception e){
+            Log.i("Exception", e.getMessage());
+        }
 
     }
 
@@ -164,4 +173,158 @@ public class CameraActivity extends Activity {
             mCamera = null;
         }
     }
-}
+
+    public void getCameraVoiceInput(){
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        speech.setRecognitionListener(CameraActivity.this);
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            speech.startListening(intent);
+            //startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Log.i("result", result.get(0));
+                    if(result.get(0).equalsIgnoreCase("Click")) {
+                        Log.i("Excuted", "In start activity");
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+
+                }
+        }
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle params) {
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onError(int error) {
+        String errorMessage = getErrorText(error);
+        Log.d("FAILED " , errorMessage);
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+
+    }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+
+    }
+
+    public static String getErrorText(int errorCode) {
+        String message;
+        switch (errorCode) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                message = "Audio recording error";
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                message = "Client side error";
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                message = "Insufficient permissions";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                message = "Network error";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                message = "Network timeout";
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                message = "No match";
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                message = "RecognitionService busy";
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                message = "error from server";
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                message = "No speech input";
+                break;
+            default:
+                message = "Didn't understand, please try again.";
+                break;
+        }
+        return message;
+    }
+
+    public class Download extends AsyncTask<String,Void,String>
+    {
+        @Override
+        protected String doInBackground(String... params) {
+            t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    Context context = getApplicationContext();
+                    if(status != TextToSpeech.ERROR) {
+                        t1.setLanguage(Locale.UK);
+                        String toSpeak = "Place hand in front of Camera and Say Click to Capture";
+                        t1.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+
+                            @Override
+                            public void onStart(String utteranceId) {
+
+                            }
+
+                            @Override
+                            public void onDone(String utteranceId) {
+                                getCameraVoiceInput();
+                            }
+
+                            @Override
+                            public void onError(String utteranceId) {
+
+                            }
+                        });
+                        t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, "1");
+                    }
+                }
+            });
+            getCameraVoiceInput();
+            return null;
+        }
+
+    }
+ }
--- app/src/main/java/com/example/team/semicolon_19/MainActivity.java
+++ app/src/main/java/com/example/team/semicolon_19/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
@@ -24,13 +26,15 @@ import java.util.Iterator;
 import java.util.Locale;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecognitionListener {
 
 
     private TextView txvResult;
     TextToSpeech t1;
     private CheckBox cBYes;
     private CheckBox cBNo;
+    public SpeechRecognizer speech;
+    private Integer onDoneCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +45,26 @@ public class MainActivity extends AppCompatActivity {
         cBYes = (CheckBox) findViewById(R.id.checkBoxYes);
         cBNo = (CheckBox) findViewById(R.id.checkBoxNo);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, 10);
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, 10);
+            }
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 11);
+            }
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 12);
+            }
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.INTERNET}, 12);
+            }
         }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 11);
-        }
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 12);
-        }
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        speech.setRecognitionListener(MainActivity.this);
 
         t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -59,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                 Context context = getApplicationContext();
                 if(status != TextToSpeech.ERROR) {
                     t1.setLanguage(Locale.UK);
-                    String toSpeak = "Please Say Yes or No";
+                    String toSpeak = "Please Say Launch Camera";
                     t1.setOnUtteranceProgressListener(new UtteranceProgressListener() {
 
                         @Override
@@ -69,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onDone(String utteranceId) {
-                            getInput();
+
                         }
 
                         @Override
@@ -81,6 +94,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Log.i("onDoneCounter", onDoneCounter.toString());
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            speech.startListening(intent);
+            //startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void getSpeechInput(View view) {
@@ -88,15 +113,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getInput(){
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, 10);
-        } else {
-            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
-        }
+        Log.i("getInput", onDoneCounter.toString());
+        onDoneCounter = 1;
     }
 
     @Override
@@ -109,25 +127,71 @@ public class MainActivity extends AppCompatActivity {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     txvResult.setText(result.get(0));
                     Log.i("result", result.get(0));
-                    if(result.get(0).equalsIgnoreCase("Yes")) {
-                        Log.i("Yes", "Yes");
-                        cBYes.setChecked(Boolean.TRUE);
-                        cBNo.setChecked(Boolean.FALSE);
-                    } else if(result.get(0).equalsIgnoreCase("No")){
-                        Log.i("No", "No");
-                        cBNo.setChecked(Boolean.TRUE);
-                        cBYes.setChecked(Boolean.FALSE);
+                    if(result.get(0).equalsIgnoreCase("Launch Camera")) {
+                        new Thread(new Runnable() {
+                            public void run() {
+                                // a potentially time consuming task
+                                Intent in = new Intent(MainActivity.this, CameraActivity.class);
+                                startActivity(in);
+                            }
+                        }).start();
                     }
-
-                    new Thread(new Runnable() {
-                        public void run() {
-                            // a potentially time consuming task
-                            Intent in = new Intent(MainActivity.this, CameraActivity.class);
-                            startActivity(in);
-                        }
-                    }).start();
 
                 }
         }
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle params) {
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onError(int error) {
+
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        Log.i("1", "onResults");
+        ArrayList<String> matches = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String text = "";
+        for (String result : matches) {
+            Log.i("onResults", result);
+            text += result + "\n";
+        }
+
+        Log.i("text", text);
+    }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+
     }
 }
